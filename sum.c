a81e2409d2ad8b7f8d6d377a0869625e8f97aa55
pragyaparashar23 <labuser@ip-172-31-8-205.ap-south-1.compute.internal>
2024-06-06 06:46:20 +0000

    Made some changes

---
+++ sum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+    printf("Enter a: ");
+    scanf("%d", &a);
+    printf("Enter b: ");
+    scanf("%d", &b);
+    c = a + b;
+    printf("Sum of numbers %d, %d is = %d\n", a, b, c);
+    return 0;
+    //Sum of two number.
+    //Now edited
+}
+  
